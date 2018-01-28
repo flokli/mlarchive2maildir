8b794ea5978fc0ac363a62eb779ddc35966dfc2b
Florian Klink <flokli@flokli.de>
2018-01-29 00:22:19 +0100

    setup.py: reorder keys, use find_packages()

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mlarchive2maildir',
+    description='Imports mail from (pipermail) archives into a maildir',
+    url='https://github.com/flokli/mlarchive2maildir',
+    author='Florian Klink',
+    author_email='flokli@flokli.de',
+    license='MIT',
     use_scm_version={'write_to': 'mlarchive2maildir/version.py'},
     setup_requires=['setuptools_scm'],
-    description='Imports mail from (pipermail) archives into a maildir',
-    install_requires=['bs4', 'requests', 'cleo', 'six'],
+    install_requires=[
+        'bs4',
+        'requests',
+        'cleo',
+        'six'
+    ],
     tests_require=[],
-    packages=['mlarchive2maildir'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': {
             'mlarchive2maildir = mlarchive2maildir.commands:main'
         }
     },
-    author='Florian Klink',
-    author_email='flokli@flokli.de',
-    license='MIT'
 )
